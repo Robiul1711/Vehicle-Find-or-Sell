66e7323cbf0c647d4630f411443933682e4fd791
ashikur221 <ashikur1603@gmail.com>
2025-09-28 10:47:33 +0600

    additional service pages done

--- src/pages/AditionalServicesPage/OtherAditionalPages/PartnerGarageDirectory.jsx
+++ src/pages/AditionalServicesPage/OtherAditionalPages/PartnerGarageDirectory.jsx
@@ -1,8 +1,47 @@
+import PartnerGarageBanner from '@/components/additionalServicesComponent/partnerGarage/PartnerGarageBanner'
+import { CommonPageWrapper } from '@/components/common/CommonPageWrapper'
 import React from 'react'
 
 const PartnerGarageDirectory = () => {
   return (
-    <div>PartnerGarageDirectory</div>
+    <div className='mt-20'>
+      <PartnerGarageBanner />
+      <CommonPageWrapper>
+
+        <div className="grid grid-cols-1  gap-4 lg:gap-10 ">
+          <div className="space-y-4">
+            <p className="lg:text-3xl font-bold">
+              Garages On LaBonneRoute.fr, you won't only find private listings. Garages also advertise their vehicles and offer you their services:
+            </p>
+
+            <ul className='list-disc pl-4'>
+              <li>
+                Sale of used vehicles with a full service history and warranty
+              </li>
+              <li>
+                Trade-in of your old vehicle
+              </li>
+              <li>
+                Maintenance and repairs
+              </li>
+              <li>
+                Pre-sale preparation and technical inspection
+              </li>
+              <li>
+                Financing solutions and mechanical warranties
+              </li>
+
+            </ul>
+            <p className="">
+              By choosing a garage, you benefit from the security of a professional and peace of mind for your purchase or sale.
+            </p>
+
+
+          </div>
+
+        </div>
+      </CommonPageWrapper>
+    </div>
   )
 }
 
--- src/pages/AditionalServicesPage/OtherAditionalPages/VirtualShowroom.jsx
+++ src/pages/AditionalServicesPage/OtherAditionalPages/VirtualShowroom.jsx
@@ -2,10 +2,12 @@ import VirtualShowroomBanner from '@/components/additionalServicesComponent/virt
 import VirtualShowroomKeyPoints from '@/components/additionalServicesComponent/virtualShowroom/VirtualShowroomKeyPoints'
 import { CommonPageWrapper } from '@/components/common/CommonPageWrapper'
 import React from 'react'
+import { ScrollRestoration } from 'react-router-dom'
 
 const VirtualShowroom = () => {
   return (
     <div className='mt-20'>
+      <ScrollRestoration />
       <VirtualShowroomBanner />
       <CommonPageWrapper>
         <VirtualShowroomKeyPoints />
